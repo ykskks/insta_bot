49d855f9a675e44fd68696a87d948aea9924ed43
= <fkt.kohei@gmail.com>
2019-10-03 11:12:01 +0900

    fix pick_article

---
+++ app.py
@@ -0,0 +1,45 @@
+import os
+
+from flask import Flask
+from instapaper import Instapaper as ipaper
+import requests
+import numpy as np
+
+
+INSTAPAPER_KEY = os.getenv('INSTAPAPER_KEY')
+INSTAPAPER_SECRET = os.getenv('INSTAPAPER_SECRET')
+EMAIL_ADRESS = os.getenv('EMAIL_ADRESS')
+PASSWORD = os.getenv('PASSWORD')
+URL = os.getenv('URL')
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return "Hello World! This is an instapaper bot!"
+
+
+@app.route('/pick_todays_article')
+def pick_article():
+    # 未読の記事と既読の記事（復習がてら）一件ずつ
+    i = ipaper(INSTAPAPER_KEY, INSTAPAPER_SECRET)
+    i.login(EMAIL_ADRESS, PASSWORD)
+
+    # 未読
+    new_article = np.random.choice(i.bookmarks(limit=500))
+    new = requests.post(URL, json={"text": new_article.url})  
+
+    id_old = i.folders()[0]["folder_id"]   
+    new_article.move(id_old) 
+
+
+    # 既読
+    old_article = np.random.choice(i.bookmarks(limit=500, folder=id_old))      
+    old = requests.post(URL, json={"text": old_article.url})  
+     
+
+
+if __name__ == '__main__':
+    app.run()
